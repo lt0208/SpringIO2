abe6b134bdbd027745768f7bbf7414120bb41061
lt0208 <lt02080208@gmail.com>
2022-12-11 17:43:40 -0800

    first

---
+++ src/main/java/com/example/demo/SpringIo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIo2Application.class, args);
+	}
+
+}
